326845f8a85efb3e5c90eb805dc697490f03c228
LIVIA ZERBINI <005376694@itaud.des.ihf>
2020-11-04 10:41:07 -0300

    alteracao

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://validacpf-livia.herokuapp.com/validacpf?cpf="
+const URL="https://validacpf-livia.herokuapp.com/?cpf="
 function validar(){
 
 	var txtCPF = document.getElementById("txtCpf").value;
